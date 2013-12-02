0f6f63976830b5cffabb788cc7b6c71ca3821f86
V <craig.crj@gmail.com>
2013-12-02 15:59:44 -0700

    Basic login page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 MathHelpQueue::Application.routes.draw do
+
+  root({:to => "sessions#login"})
+  get "sessions/login", to: "sessions#login"
+  post "sessions/login_attempt", to: "sessions#login_attempt"
+  #get "sessions/home"
+  #get "sessions/profile"
+  #get "sessions/setting"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
