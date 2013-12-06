85fcf9c8f154f089ef15cec391533ebca257b1e7
V <craig.crj@gmail.com>
2013-12-06 14:51:36 -0700

    Some initial data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+courses = [{:title => "Calculus 1", :discipline => "Math", :number => 112}, 
+           {:title => "Calculus 2", :discipline => "Math", :number => 113}, 
+           {:title => "Fundamentals of Mathematics", :discipline => "Math", :number => 290}, 
+           {:title => "Linear Algebra", :discipline => "Math", :number => 313}, 
+           {:title => "Multivariable Calculus", :discipline => "Math", :number => 314}, 
+           {:title => "Differential Equations", :discipline => "Math", :number => 334}, 
+           {:title => "Introduction to Programming", :discipline => "C S", :number => 142}, 
+           {:title => "Data Structures", :discipline => "C S", :number => 235}, 
+           {:title => "Discrete Structure", :discipline => "C S", :number => 236}, 
+           {:title => "Advanced Programming Concepts", :discipline => "C S", :number => 240}, 
+           {:title => "Internet Programming", :discipline => "C S", :number => 360}]
+courses.each do |course|
+  Course.create(course)
+end
+
+
+users = [{:username => "Craig"}, 
+         {:username => "Ryan"}, 
+         {:username => "Adam"}, 
+         {:username => "Jeremy"}]
+users.each do |user|
+  User.create(user)
+end
